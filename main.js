838745c5a05daf42be56232ba469ec368cf738a5
Sandro Pachas <sandroromanipr24@gmail.com>
2026-04-16 20:31:33 -0500

    Agregando secciones principales de la pagina

---
+++ main.js
@@ -0,0 +1,180 @@
+const datosJuegos = {
+    accion: [
+        {
+            titulo: "assassin's creed ii",
+            img: "https://static.wikia.nocookie.net/theassassinscreed/images/4/41/Ac2cover.jpg/revision/latest/thumbnail/width/360/height/360?cb=20100918031210&path-prefix=es",
+            desc: "Ezio Auditore inicia su camino como asesino en la Italia del Renacimiento después de una tragedia familiar. Esta entrega marca el inicio de una de las historias más queridas de la saga.",
+            video: "https://www.youtube.com/embed/LGC88YwrfkM"
+        },
+        {
+            titulo: "assassin's creed brotherhood",
+            img: "https://upload.wikimedia.org/wikipedia/en/2/2a/Assassins_Creed_brotherhood_cover.jpg",
+            desc: "Ezio llega a Roma y lidera la hermandad de asesinos para enfrentar a los Borgia. Aquí se consolida como mentor y protector de la Orden.",
+            video: "https://www.youtube.com/embed/b2Ay0plpImA"
+        },
+        {
+            titulo: "assassin's creed revelations",
+            img: "https://upload.wikimedia.org/wikipedia/en/d/d9/Assassins_Creed_Revelations_Cover.jpg",
+            desc: "Un Ezio más sabio viaja a Constantinopla para descubrir secretos del pasado y cerrar su historia de manera memorable.",
+            video: "https://www.youtube.com/embed/yTVX9GulYdg"
+        },
+        {
+            titulo: "tomb raider (2013)",
+            img: "https://static.wikia.nocookie.net/tomb-raider/images/1/11/TRde_Portada_cuadrada.png/revision/latest/scale-to-width/360?cb=20240721192939&path-prefix=es",
+            desc: "Lara Croft sobrevive a una isla peligrosa y da sus primeros pasos como aventurera en una historia de supervivencia y valentía.",
+            video: "https://www.youtube.com/embed/M4SG6DfVvLs"
+        },
+        {
+            titulo: "rise of the tomb raider",
+            img: "https://image.api.playstation.com/cdn/UP0082/CUSA05794_00/HzYMHoXoANNfVJ5BlxcTJ8bpCHCW3j8j.png",
+            desc: "Lara continúa su búsqueda de secretos antiguos y enfrenta nuevos peligros mientras crece como exploradora.",
+            video: "https://www.youtube.com/embed/1_FIyNcQSgA"
+        },
+        {
+            titulo: "shadow of the tomb raider",
+            img: "https://store-images.s-microsoft.com/image/apps.51344.69361434237560300.08369373-2b5b-4056-89d1-c943a5bfd11b.5baa2661-0f85-45ba-b621-996e640fc265",
+            desc: "Lara enfrenta las consecuencias de sus decisiones y completa su transformación en la heroína legendaria de la saga.",
+            video: "https://www.youtube.com/embed/XYtyeqVQnRI"
+        }
+    ],
+    mundoAbierto: [
+        {
+            titulo: "just cause",
+            img: "https://upload.wikimedia.org/wikipedia/en/e/e4/Just-Cause-win-cover.jpg",
+            desc: "Rico Rodriguez inicia su misión como agente de la CIA para derrocar gobiernos corruptos en una isla caribeña. El inicio de la saga más explosiva del mundo abierto.",
+            video: "https://www.youtube.com/embed/wJ-CvJsgG2s"
+        },
+        {
+            titulo: "just cause 2",
+            img: "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Just_Cause_2_cover_art.jpg/250px-Just_Cause_2_cover_art.jpg",
+            desc: "Rico viaja a Panau, una isla controlada por un dictador. Con el garfio como herramienta principal, la libertad de movimiento y destrucción alcanza un nuevo nivel.",
+            video: "https://www.youtube.com/embed/oTXqYg_S4Ps"
+        },
+        {
+            titulo: "just cause 3",
+            img: "https://image.api.playstation.com/cdn/UP0082/CUSA01493_00/PZic3LvHOpeMPSmfBTQXg3LsIkkayPgW.png?w=440",
+            desc: "Rico regresa a su tierra natal, Medici, para liberar a su pueblo del dictador Di Ravello. El caos, las explosiones y el wingsuit llevan la saga a su máxima expresión.",
+            video: "https://www.youtube.com/embed/n49TPURRn38"
+        },
+        {
+            titulo: "far cry 3",
+            img: "https://gamescenter.pe/wp-content/uploads/2024/08/Far-Cry-3.jpg",
+            desc: "Jason Brody queda atrapado en una isla controlada por el carismático villano Vaas Montenegro. Una historia de supervivencia y locura en uno de los mejores mundos abiertos de su generación.",
+            video: "https://www.youtube.com/embed/J6gnOVJsCsM"
+        },
+        {
+            titulo: "far cry 4",
+            img: "https://upload.wikimedia.org/wikipedia/en/6/63/Far_Cry_4_box_art.jpg",
+            desc: "Ajay Ghale regresa a Kyrat, su tierra natal en el Himalaya, para cumplir la última voluntad de su madre y se ve envuelto en una guerra civil contra el excéntrico dictador Pagan Min.",
+            video: "https://www.youtube.com/embed/6d60v1OErEY"
+        },
+        {
+            titulo: "far cry 5",
+            img: "https://blizzstoreperu.com/cdn/shop/files/farcry5peru.jpg?v=1728259437",
+            desc: "En el condado de Hope, Montana, un culto religioso armado toma el control. El jugador debe liberar la región enfrentando a Joseph Seed y sus seguidores fanáticos.",
+            video: "https://www.youtube.com/embed/4hVME3O9wf4"
+        },
+        {
+            titulo: "mafia: the city of lost heaven",
+            img: "https://m.media-amazon.com/images/M/MV5BZGVlNzgwMWItZGU4Zi00ZWQ0LWJmZjctZGM2NmNlZmVmNmE3XkEyXkFqcGc@._V1_.jpg",
+            desc: "Tommy Angelo, un taxista de los años 30, se convierte en miembro de la familia Salieri. Una historia de lealtad, traición y el precio de la vida criminal.",
+            video: "https://www.youtube.com/embed/-Hs7Y0gzQ7E"
+        },
+        {
+            titulo: "mafia ii",
+            img: "https://upload.wikimedia.org/wikipedia/en/0/0d/Mafia_II_Boxart.jpg",
+            desc: "Vito Scaletta regresa de la Segunda Guerra Mundial y encuentra en el crimen organizado su camino al éxito. Ambientado en los años 40-50.",
+            video: "https://www.youtube.com/embed/wHoqwphzhzc"
+        },
+        {
+            titulo: "mafia iii",
+            img: "https://gamescenter.pe/wp-content/uploads/2024/08/Mafia-3.jpg",
+            desc: "Lincoln Clay, veterano de Vietnam, busca venganza contra la mafia italiana que traicionó a su familia en Nueva Orleans de los años 60.",
+            video: "https://www.youtube.com/embed/TNiHOpHbCzM"
+        }
+    ],
+    terror: [
+        {
+            titulo: "resident evil (remake)",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEYrCQbLKVlSWvcOSEbbvVAY3FirsCsX4oCg&s",
+            desc: "Los miembros del equipo S.T.A.R.S. quedan atrapados en una mansión infestada de criaturas mutantes creadas por la corporación Umbrella.",
+            video: "https://www.youtube.com/embed/IjxFtFwY6jk"
+        },
+        {
+            titulo: "resident evil 2 (remake)",
+            img: "https://upload.wikimedia.org/wikipedia/en/f/fd/Resident_Evil_2_Remake.jpg",
+            desc: "Leon Kennedy y Claire Redfield llegan a Raccoon City en medio de un apocalipsis zombie. El implacable Mr. X los persigue sin descanso.",
+            video: "https://www.youtube.com/embed/u3wS-Q2KBpk"
+        },
+        {
+            titulo: "resident evil 3 (remake)",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaRlfsg4ZStzlxaasfYIdBcTpfRjuoou3pfg&s",
+            desc: "Jill Valentine intenta escapar de Raccoon City perseguida por Nemesis, una bioarma casi indestructible. Una carrera frenética por sobrevivir.",
+            video: "https://www.youtube.com/embed/djjHZoDIvSY"
+        },
+        {
+            titulo: "bioshock",
+            img: "https://gamescenter.pe/wp-content/uploads/2024/08/Bioshock-The-Collection.jpg",
+            desc: "Un avión cae en el océano y el protagonista descubre Rapture, una ciudad submarina utópica convertida en pesadilla. Una obra maestra del diseño.",
+            video: "https://www.youtube.com/embed/rrqfPG4ZcAA"
+        },
+        {
+            titulo: "bioshock 2",
+            img: "https://upload.wikimedia.org/wikipedia/en/8/8c/Bioshock2_cover.png",
+            desc: "El jugador controla a un Big Daddy que busca a su Little Sister en una Rapture aún más decadente. Explora el colectivismo.",
+            video: "https://www.youtube.com/embed/tzWTcDydlpM"
+        },
+        {
+            titulo: "bioshock infinite",
+            img: "https://upload.wikimedia.org/wikipedia/en/a/a3/Official_cover_art_for_Bioshock_Infinite.jpg",
+            desc: "Booker DeWitt viaja a Columbia, una ciudad flotante en las nubes, para rescatar a Elizabeth. Una historia de realidades paralelas.",
+            video: "https://www.youtube.com/embed/WYRePGtK074"
+        }
+    ],
+    shooter: [
+        {
+            titulo: "halo: combat evolved",
+            img: "https://upload.wikimedia.org/wikipedia/en/8/80/Halo_-_Combat_Evolved_%28XBox_version_-_box_art%29.jpg",
+            desc: "El Master Chief despierta en un anillo alienígena llamado Halo. Debe detener al Covenant y descubrir el terrible secreto que guarda la instalación.",
+            video: "https://www.youtube.com/embed/efThjRym-ks"
+        },
+        {
+            titulo: "halo 2",
+            img: "https://upload.wikimedia.org/wikipedia/en/9/92/Halo2-cover.png",
+            desc: "La guerra llega a la Tierra. Además de Master Chief, el jugador controla al Árbitro, un guerrero Sangheili. Revolucionó el multijugador.",
+            video: "https://www.youtube.com/embed/4ycOwVPqbxA"
+        },
+        {
+            titulo: "halo 3",
+            img: "https://store-images.s-microsoft.com/image/apps.32635.13744479854832406.0affa1b0-655f-49af-ab66-9c9ccd71090c.75be220c-443f-4f73-b3af-4dfcd5da2e9c",
+            desc: "El cierre épico de la trilogía original. Master Chief regresa para detener el Diluvio y al Profeta de la Verdad en una batalla final.",
+            video: "https://www.youtube.com/embed/4ycOwVPqbxA"
+        }
+    ]
+};
+
+function crearTarjetas(categoria, contenedorId) {
+    const contenedor = document.getElementById(contenedorId);
+    let htmlFinal = "";
+    datosJuegos[categoria].forEach(juego => {
+        htmlFinal += `
+            <div class="game-card">
+                <img src="${juego.img}" alt="${juego.titulo}" loading="lazy">
+                <div class="game-info">
+                    <div class="title-box">${juego.titulo}</div>
+                    <div class="description-box">${juego.desc}</div>
+                </div>
+                <div class="video-box">
+                    <iframe src="${juego.video}" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>`;
+    });
+    contenedor.innerHTML = htmlFinal;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    crearTarjetas("accion", "contenedor-accion");
+    crearTarjetas("mundoAbierto", "contenedor-mundo-abierto");
+    crearTarjetas("terror", "contenedor-terror");
+    crearTarjetas("shooter", "contenedor-shooter");
+});
